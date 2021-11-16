da10f89d8acb8eec8314ab62db17ef97f9f1c7e3
anonymous anonymous <nishant.dhanendra@kellogg.northwestern.edu>
2021-11-16 04:47:51 +0000

    Whitelist columns in Viewer for ActiveAdmin

--- app/admin/viewers.rb
+++ app/admin/viewers.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Viewer do
 
+ permit_params :email, :password, :movie_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
