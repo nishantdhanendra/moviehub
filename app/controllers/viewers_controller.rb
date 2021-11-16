4589725334435f7c409ef17fd6f7ee80423f300f
anonymous anonymous <nishant.dhanendra@kellogg.northwestern.edu>
2021-11-16 04:48:16 +0000

    Install Kaminari

--- app/controllers/viewers_controller.rb
+++ app/controllers/viewers_controller.rb
@@ -3,7 +3,7 @@ class ViewersController < ApplicationController
 
   # GET /viewers
   def index
-    @viewers = Viewer.all
+    @viewers = Viewer.page(params[:page]).per(10)
   end
 
   # GET /viewers/1
